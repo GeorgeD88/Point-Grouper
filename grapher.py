8ef5cb008dcc604581626f2d05d50a9ad9437532
George Doujaiji <GeorgeD.88888@gmail.com>
2019-10-16 09:50:17 -0400

    Add x, y axis lines

--- grapher.py
+++ grapher.py
@@ -3,7 +3,7 @@ import numpy as np
 import grouper
 
 
-points = []  # Initial list of ordered pairs.
+points = [(-1, 3), (2, -4), (1, 0), (3, -2), (-1, 0)]  # Initial list of ordered pairs.
 """"
 Points is the list of points you want to average, each list item is an ordered pair (x, y).
 Replace the empty list in the points variable with a list of tuples, which are ordered pairs.
@@ -16,8 +16,24 @@ for x, y in points:  # Appends all points from the list to their respective list
     x_points.append(x)
     y_points.append(y)
 
-
 fig, ax = plt.subplots(1, 1)
+
+# X, Y AXIS CODE V
+# set the x-spine (see below for more info on `set_position`)
+ax.spines['left'].set_position('zero')
+
+# turn off the right spine/ticks
+ax.spines['right'].set_color('none')
+ax.yaxis.tick_left()
+
+# set the y-spine
+ax.spines['bottom'].set_position('zero')
+
+# turn off the top spine/ticks
+ax.spines['top'].set_color('none')
+ax.xaxis.tick_bottom()
+# X, Y AXIS CODE ^
+
 plt.scatter(np.asarray(x_points), np.asarray(y_points), c='black')
 plt.scatter(*np.asarray(grouper.get_average(points)), c='red')
 
